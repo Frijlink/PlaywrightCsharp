405b090acceae319e35d4b7db95266a5549c0920
Hein-Carl Frijlink <hc.frijlink@freedom.nl>
2023-10-04 16:44:07 +0200

    update locators

--- SupportCode/Pages/Components/HeaderComponent.cs
+++ SupportCode/Pages/Components/HeaderComponent.cs
@@ -11,7 +11,7 @@ public class HeaderComponent
     public HeaderComponent(IPage page)
     {
         _page = page;
-        _logInBtn = _page.Locator("css=a[class^=\"Buttonsstyles\"][href=\"/login\"]");
+        _logInBtn = _page.Locator("css=a[class^=\"Buttonsstyles\"][href*=\"/login\"]");
         _memberInfoBtn = _page.Locator("css=[data-testid=\"header-member-menu-button\"]");
         _memberInfoLogoutBtn = _page.Locator("css=[data-testid=\"account-menu-logout\"]");
         _logoutSubmitBtn = _page.Locator("css=button#logout-submit");
--- SupportCode/Pages/Trello/LoginPage.cs
+++ SupportCode/Pages/Trello/LoginPage.cs
@@ -5,22 +5,20 @@ public class LoginPage
     private readonly IPage _page;
     private readonly ILocator _userInput;
     private readonly ILocator _passwordInput;
-    private readonly ILocator _loginBtn;
     private readonly ILocator _loginSubmitBtn;
 
     public LoginPage(IPage page)
     {
         _page = page;
-        _userInput = _page.Locator("css=input#user");
+        _userInput = _page.Locator("css=input#username");
         _passwordInput = _page.Locator("css=input#password");
-        _loginBtn = _page.Locator("css=input#login");
         _loginSubmitBtn = _page.Locator("css=button#login-submit");
     }
 
     public async Task Login(string user, string password)
     {
         await _userInput.FillAsync(user);
-        await _loginBtn.ClickAsync();
+        await _loginSubmitBtn.ClickAsync();
         await _passwordInput.FillAsync(password);
         await _loginSubmitBtn.ClickAsync();
     }
